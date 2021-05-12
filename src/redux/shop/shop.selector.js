67fc0ebdb0437bb3a89dd820fb327d6e3672641d
Raviraj <jariwalaraviraj@gmail.com>
2021-05-12 16:29:37 +0530

    minor fixes

---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,29 @@
+import { createSelector } from "reselect";
+
+export const SelectShopData = (state) => state.shop;
+
+export const SelectShopCollection = createSelector(
+  [SelectShopData],
+  (shop) => shop.collections
+);
+
+export const SelectCollectionPreview = createSelector(
+  [SelectShopCollection],
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
+);
+
+export const SelectCollection = (collectionUrlParam) =>
+  createSelector([SelectShopCollection], (collections) =>
+    collections ? collections[collectionUrlParam] : null
+  );
+
+export const selectIsCollectionsFetching = createSelector(
+  [SelectShopData],
+  (shop) => shop.isFetching
+);
+
+export const selectIsCollectionsloaded = createSelector(
+  [SelectShopData],
+  (shop) => !!shop.collections
+);
